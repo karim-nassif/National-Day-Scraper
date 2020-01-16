5b291917e915ff9ad36f2991093f9c70244dc1b3
Karim Nassif <karim.nassif@vividseats.com>
2020-01-16 14:51:20 -0600

    filter non-ascii characters

--- national_day.py
+++ national_day.py
@@ -2,8 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 
+def strip_non_ascii(string):
+    ''' Returns the string without non ASCII characters'''
+    stripped = (c for c in string if 0 < ord(c) < 127)
+    return ''.join(stripped)
+
 d = datetime.datetime.today();
-day = d.strftime('%d')
+day = '17'
 month = d.strftime('%B')
 
 url = "https://nationaltoday.com/"+month.lower()+"-"+day+"-holidays"
@@ -15,8 +20,7 @@ rows = soup.find_all('table')[0].findChildren(['tr'])
 dayList = []
 
 for row in rows[1:]:
-	dayList.append(row.findAll('td')[1].text)
+	dayList.append(strip_non_ascii(row.findAll('td')[1].text))
 
 days = ', '.join(map(str, dayList))
 print days
-
